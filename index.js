807a5040cb1e89d7eed94df71997521186134661
yzrsamet <yzr.samet@gmail.com>
2020-11-16 22:04:05 +0100

    first get request

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+// Requires the Express module just as you require other modules and and puts it in a variable.
 
 //set up express app
 const app = express();
+// puts new Express application inside the app variable
+
+app.get('/',function(req,res){
+    console.log('GET requests');
+    //res.end();
+    res.send({name: 'Samet'});
+});
 
 //listen for request
 app.listen(process.env.port || 4000,function(){
